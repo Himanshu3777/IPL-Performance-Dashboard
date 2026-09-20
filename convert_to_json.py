db35b3bffdd5602e5a1092a5d65b68e77e6771c8
Himanshu Yadav <himanshuyadav88813@gmail.com>
2026-09-20 21:54:01 +0530

    Optimize JSON (87MB to 75MB) + Add WhiteNoise for Render production

--- convert_to_json.py
+++ convert_to_json.py
@@ -1,4 +1,3 @@
-# convert_to_json.py
 import csv
 import json
 import os
@@ -6,7 +5,6 @@ import os
 CSV_FILE = 'ipl-ball-by-ball-dataset.csv'
 JSON_FILE = 'dashboard/static/dashboard/data/ipl_data.json'
 
-# Folder auto-create karega (Django standard)
 os.makedirs(os.path.dirname(JSON_FILE), exist_ok=True)
 
 data = []
@@ -15,14 +13,12 @@ with open(CSV_FILE, 'r', encoding='utf-8') as f:
     for row in reader:
         data.append({
             'match_id': row.get('match_id', ''),
-            'season': row.get('season', ''),
             'team1': row.get('team1', ''),
             'team2': row.get('team2', ''),
             'winner': row.get('winner', ''),
             'batting_team': row.get('batting_team', ''),
             'batter': row.get('batter', ''),
             'bowler': row.get('bowler', ''),
-            'non_striker': row.get('non_striker', ''),
             'runs_batter': row.get('runs_batter', '0'),
             'runs_total': row.get('runs_total', '0'),
             'wicket_kind': row.get('wicket_kind', ''),
